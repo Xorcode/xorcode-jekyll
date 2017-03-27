713484e4976f92a9ac41fe83023e591829297e85
Torgny Bjers <torgny@xorcode.com>
2017-03-27 15:50:13 -0400

    Activating jekyll-admin only in development mode

--- _plugins/ext.rb
+++ _plugins/ext.rb
@@ -1,1 +1,9 @@
 require 'jekyll/tagging'
+
+gem_name, *gem_ver_reqs = 'jekyll-admin', '~> 0.4.0'
+gdep = Gem::Dependency.new(gem_name, *gem_ver_reqs)
+found_gspec = gdep.matching_specs.max_by(&:version)
+if found_gspec
+  puts 'Activating Jekyll Admin (development)'
+  require 'jekyll-admin'
+end
